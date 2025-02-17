e76059b1233ae786332786c755df41131e3622fa
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2025-02-17 10:25:55 +0800

    Create download_fb.py

---
+++ download_fb.py
@@ -0,0 +1,19 @@
+  import yt_dlp
+import sys
+
+def download_facebook_video(url, output_path="downloads"):
+    ydl_opts = {
+        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+        'format': 'bestvideo+bestaudio/best'
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python download_fb.py <Facebook Video URL>")
+        sys.exit(1)
+
+    fb_video_url = sys.argv[1]
+    download_facebook_video(fb_video_url)
